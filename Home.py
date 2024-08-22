200d4820f3eb208806e4630b90d0b7a386c4a133
Magariou Diallo <diallomagariou@gmail.com>
2024-08-22 09:02:52 -0400

    Update Home Page and Readme Page.

--- Home.py
+++ Home.py
@@ -26,5 +26,15 @@ st.markdown(
 
     Explorez les différentes sections de l'application pour découvrir mes analyses exploratoires et prédictions personnalisées.
 
+    ---
+    ### Apropos des données utilisées
+    L'ensemble de données utilisé contient des informations complètes sur 2 392 élèves du secondaire  ou du lycée dépendamment du système académique (high school).
+    Il détaille les caractéristiques démographiques des étudiants (comme l'âge, le sexe, l'ethnie, ...), leurs habitudes d'étude (temps d'apprentissage par semaine, nombre d'absences, ...), 
+    l'implication des parents , les activités parascolaires (sports, bénévolat, musique, ...) et les résultats scolaires (note moyenne cumulative comprise entre 2.00 à 4.00). 
+    La moyenne cumulative est influencée par les différentes caractéristiques enumérées ci-dessus. La variable cible, GradeClass, classe les notes des élèves en catégories distinctes selon la
+    moyenne cumulative. Voici les différentes classes : **A : Excellent, B : Très bien, C : Bien, D : Passable et F : Echec**.
+
+    La source complète des données est disponible ici: https://www.kaggle.com/datasets/rabieelkharoua/students-performance-dataset.
+
 """
 )
